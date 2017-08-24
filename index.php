c9dca1c2d100afb8e25f0304099430758e384833
zhangwei <664886892@qq.com>
2017-08-24 09:47:18 +0800

    ok

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
   require __DIR__ ."/vendor/autoload.php";
+  $gates=[
+      'BaiYun_A_A17',
+      'BeiJing_J7',
+      'ShuangLiu_K203',
+      'HongQiao_A157',
+      'A2',
+      'BaiYun_B_B230'
+  ];
+
+  $boards=collect($gates)->map(function($gate){
+      return collect(explode('_',$gate))->last();
+  })->toArray();
+  var_dump($boards);
